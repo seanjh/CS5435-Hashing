eca7a986cd8959fc2b9f60a9b80500d54945d23e
Sean Herman <seanherman@gmail.com>
2015-09-09 00:13:44 -0400

    question 4.3

---
+++ crack_medallion_hash.py
@@ -0,0 +1,37 @@
+import csv
+import re
+import hashlib
+
+LLLDDD_FORMAT = re.compile(r'[A-Z]{3}\d{3}')
+
+def read_target_digests():
+  with open('medallion_hashes.txt', encoding='utf-8') as infile:
+    return [h.strip() for h in list(infile)]
+
+def medallion_matches_format(medallion, pattern):
+  return pattern.match(medallion)
+
+def medallions():
+  with open('medallions.csv', newline='', encoding='utf-8') as infile:
+    reader = csv.reader(infile, delimiter=',', quotechar='"')
+    fieldnames = next(reader)
+    for line in reader:
+      data = dict(zip(fieldnames, line))
+      medallion = data["License Number"]
+      if medallion_matches_format(medallion, LLLDDD_FORMAT):
+        yield data
+
+def hash_medallion(medallion):
+  return hashlib.sha256(bytes(medallion, 'utf-8')).hexdigest()
+
+def main():
+  targets = frozenset(read_target_digests())
+  for line in medallions():
+    digest = hash_medallion(line["License Number"])
+    if digest in targets:
+      print('Matched `%s` to medallion `%s` owned by %s' % (
+        digest, line["License Number"], line["Name of Licensee"]
+      ))
+
+if __name__ == '__main__':
+  main()
